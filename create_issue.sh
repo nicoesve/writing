1908f9f306f3583e42f21883fda6c842075ae9a0
Nicolas Escobar <nicoesve@gmail.com>
2023-12-15 19:40:00 +0000

    creating issues

---
+++ create_issue.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TITLE=$1
+
+# Run the Python script
+python3 issue_body.py
+
+# Open issue_body.md in nvim and wait for it to be closed
+nvim issue_body.md
+
+# Get the content of issue_body.md
+BODY=$(cat issue_body.md)
+
+# Create issue with gh
+gh issue create --title "$TITLE" --body "$BODY"
+
---
+++ issue_body.py
@@ -0,0 +1,25 @@
+import yaml
+
+# Prompt for details
+deadline = input("Enter deadline: ")
+length = input("Enter length: ")
+previous = input("Enter previous: ")
+next_step = input("Enter next step: ")
+budget = input("Enter budget: ")
+
+# Prepare data
+data = {
+    'deadline': deadline,
+    'length': length,
+    'previous': previous,
+    'next': next_step,
+    'budget': budget
+}
+
+# Convert data to yaml
+yaml_data = yaml.safe_dump(data, explicit_start=True, explicit_end=True)
+
+# Write yaml data to markdown file
+with open('issue_body.md', 'w') as f:
+    f.write(yaml_data)
+
